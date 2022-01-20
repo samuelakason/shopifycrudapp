9c4fd87e930a4a9727201dfb6fbf03c01508f19f
samuelakason <93771056+samuelakason@users.noreply.github.com>
2022-01-20 12:36:01 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,16 +1,19 @@
 <?php
 
-'mysql'  => [
-    'driver'    => 'mysql',
-    'host'      => env('DB_HOST', 'us-cdbr-east-05.cleardb.net'),
-    'database'  => env('DB_DATABASE', 'heroku_4e5de3c41942bd9'),
-    'username'  => env('DB_USERNAME', 'bb657731e3b647'),
-    'password'  => env('DB_PASSWORD', '584ac29b'),
-    'charset'   => 'utf8',
-    'collation' => 'utf8_unicode_ci',
-    'prefix'    => '',
-    'strict'    => false,
-],
+$servername = "localhost";
+
+$username = "root"; 
+
+$password = ""; 
+
+$dbname = "xenex_crud"; 
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+
+    die("Connection failed: " . $conn->connect_error);
+
+}
 
 ?> 
-
